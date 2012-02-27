5388c5b7fbe20066bcabe7b8537e2408562a6d55
Zack Piispanen <olink@shankshock.com>
2012-02-27 16:39:26 -0500

    Token commit

--- Schematics/Schematics.cs
+++ Schematics/Schematics.cs
@@ -30,7 +30,7 @@ namespace Schematics
 
         public override Version Version
         {
-            get { return new Version("1.0.0.0"); }
+            get { return new Version("1.0.0.1"); }
         }
 
         public Schematics(Main game) : base(game)
