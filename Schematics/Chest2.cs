4a5fbc4a3eeb4e7a01b36a8aef1e996a078f60f6
Zack Piispanen <olink@shankshock.com>
2012-02-27 15:13:49 -0500

    Chest save commit

---
+++ Schematics/Chest2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+
+namespace Schematics
+{
+    [Serializable()]
+    public struct Chest2
+    {
+        public Item2[] item;
+        public int x;
+        public int y;
+
+        public Chest2( int mi, int x, int y )
+        {
+            item = new Item2[mi];
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Schematics/Item2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Schematics
+{
+    [Serializable()]
+    public struct Item2
+    {
+        public int netId;
+        public int stack;
+
+        public Item2( int netId, int stack)
+        {
+            this.netId = netId;
+            this.stack = stack;
+        }
+    }
+}
